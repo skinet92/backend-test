9fbae1d6705a3087a0fe77292d0b9d59a10f8660
Miguel Estevez <miguelestebane92@gmail.com>
2020-09-27 14:47:44 +0200

    fixing entity

--- src/Item.php
+++ src/Item.php
@@ -2,20 +2,70 @@
 
 namespace Runroom\GildedRose;
 
-class Item {
+class Item
+{
 
     public $name;
     public $sell_in;
     public $quality;
 
-    function __construct($name, $sell_in, $quality) {
+    public function __construct($name, $sell_in, $quality)
+    {
         $this->name = $name;
         $this->sell_in = $sell_in;
         $this->quality = $quality;
     }
 
-    public function __toString() {
-        return "{$this->name}, {$this->sell_in}, {$this->quality}";
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
     }
 
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSellIn()
+    {
+        return $this->sell_in;
+    }
+
+    /**
+     * @param mixed $sell_in
+     */
+    public function setSellIn($sell_in): void
+    {
+        $this->sell_in = $sell_in;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getQuality()
+    {
+        return $this->quality;
+    }
+
+    /**
+     * @param mixed $quality
+     */
+    public function setQuality($quality): void
+    {
+        $this->quality = $quality;
+    }
+
+    public function __toString()
+    {
+        return "{$this->name}, {$this->sell_in}, {$this->quality}";
+    }
 }
